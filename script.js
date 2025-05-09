d2613d03e28db5c4adbd329f738cdd7086876729
IsaqueLucas17121 <lucas.mourao2016@gmail.com>
2025-05-08 21:45:21 -0300

    Slide Finalizado (loja) 08/05

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* Loja */
+
 function MudarPagina(index) {
     const botoes = [
         document.getElementById("pagi1"),
@@ -16,3 +18,39 @@ function MudarPagina(index) {
     }
 }
 
+/* Slide */
+
+
+var radio = document.querySelector('.manual-btn')
+var contador = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaimg()
+}, 5000)
+
+
+// rolamento do slide 
+
+function proximaimg(){
+
+    contador++
+
+    if(contador > 3)(
+        contador = 1
+    )
+
+    document.getElementById('radio'+contador).checked = true
+
+    
+}
+
+// mudar imagem slide
+
+function mudarImg(index){
+    for( i=1; i<=4 ; i++ ){
+        document.getElementById("img_principal" + i).classList.add("desligado")
+    }
+    document.getElementById("img_principal" + index).classList.remove("desligado")
+}
