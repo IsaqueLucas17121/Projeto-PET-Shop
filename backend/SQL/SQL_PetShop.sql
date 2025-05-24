dab9497fb6888f86db31d088b40799e29195daea
Caio <caioquerino04@gmail.com>
2025-05-24 10:27:20 -0300

    Métodos dentro de uma class PHP 24/05

--- backend/SQL/SQL_PetShop.sql
+++ backend/SQL/SQL_PetShop.sql
@@ -2,20 +2,17 @@ create database PetShop;
 
 use PetShop;
 
-select * from usuario;
+select * from usuarios;
 
-create table endereco(
+create table enderecos(
 cep varchar(255) not null primary key,
-numero varchar(255),
 rua varchar(255),
 bairro varchar(255),
 cidade varchar(255),
 estado varchar(255),
-complemento varchar(255)
-
 );
 
-create table usuario(
+create table usuarios(
 cpf varchar(255) not null primary key,
 logado varchar(255),
 nome varchar(255),
@@ -25,45 +22,56 @@ senha varchar(255),
 img varchar(255),
 celular varchar(255),
 cep varchar(255),
+numero varchar(255),
+complemento varchar(255),
 
-constraint fk_cep_usu foreign key (cep) references endereco(cep)
+constraint fk_cep_usu foreign key (cep) references enderecos(cep)
 );
 
-create table pet(
-id_pet int primary key not null auto_increment,
+create table pets(
+identidade varchar(255) not null primary key,
 nome varchar(255),
 tipo varchar(255),
 raca varchar(255),
-id_usu varchar(255) not null,
-constraint fk_pet_usu foreign key (id_usu) references usuario(cpf)
+idUsuario varchar(255) not null,
+constraint fk_pet_usu foreign key (idUsuario) references usuarios(cpf)
 );
 
-create table vendedor(
-id_ven varchar(255) not null primary key,
+create table funcionarios(
+idFuncionario varchar(255) not null primary key,
 nome varchar(255),
 sobrenome varchar(255),
-funcao varchar(255),
+funcao enum("Caixa", "Veterinário", "Gerente", "Master"),
 email varchar(255),
 senha varchar(255)
+cep varchar(255),
+numero varchar(255),
+complemento varchar(255),
+
+constraint fk_cep_funcionario foreign key (cep) references endereco(cep)
 );
 
-create table loja(
-id_loja int not null auto_increment primary key,
+create table lojas(
+idLoja int not null auto_increment primary key,
 nome varchar(255),
 img varchar(255)
+cep varchar(255),
+numero varchar(255),
+complemento varchar(255),
+
+constraint fk_cep_loja foreign key (cep) references endereco(cep)
 );
 
-create table produto(
-id_pro int not null auto_increment primary key,
+create table produtos(
+idPro int not null auto_increment primary key,
 nome varchar(255),
 descricao varchar(255),
-valor varchar(255),
+preco varchar(255),
 img varchar(255),
-id_loja int,
-id_usu varchar(255),
-constraint fk_id_loja foreign key (id_loja) references loja(id_loja),
-constraint fk_pro_usu foreign key (id_usu) references usuario(cpf)
-
+idLoja int,
+idFuncionario varchar(255),
+constraint fk_id_loja foreign key (idLoja) references loja(idLoja),
+constraint fk_pro_funcionario foreign key (idFuncionario) references funcionario(idFuncionario)
 );
 
 
