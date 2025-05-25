8f9da6be13003c58494183e12f5494fcb0fbd1ad
Caio <caioquerino04@gmail.com>
2025-05-25 20:42:54 -0300

    ... 25/05

---
+++ backend/php/config.php
@@ -0,0 +1,10 @@
+<?php
+    $DbHost = "localhost";
+    $DbUsername = "root";
+    $DbPassword = "";
+    $DbName = "PetShop";
+<<<<<<< HEAD
+?>
+=======
+?>
+>>>>>>> c706b11d115b3a09321d0e62d2383ccf3c045bfb
